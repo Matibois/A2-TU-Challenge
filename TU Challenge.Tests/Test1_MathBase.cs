734575b68e70de15cf7cebbb88def1be5bcbb20a
mattis.leconte@gmail.com <mattis.leconte@gmail.com>
2023-01-27 16:02:38 +0100

    oui

---
+++ TU Challenge.Tests/Test1_MathBase.cs
@@ -0,0 +1,22 @@
+﻿namespace TU_Challenge
+{
+    public class Test1_MathBase
+    {
+
+        /// <summary>
+        /// Test pas obligatoire mais essayez un peu quand même. N'hésitez pas à me demander de l'aide
+        /// </summary>
+        [Test]
+        public void GenericSort()
+        {
+            var toSort = new List<int>() { 12, 0, -1, 123, 45, 90, -123 };
+
+            var result = MyMathImplementation.GenericSort(toSort, MyMathImplementation.IsInOrder);
+
+            for (int i = 0; i < result.Count - 1; i++)
+            {
+                Assert.IsTrue(result[i] < result[i + 1]);
+            }
+        }
+    }
+}
--- TU Challenge/MyMathImplementation.cs
+++ TU Challenge/MyMathImplementation.cs
@@ -8,6 +8,86 @@ namespace TU_Challenge
 {
     public class MyMathImplementation
     {
-        
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static List<int> GenericSort(List<int> toSort, object isInOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<int> GetAllPrimary(int a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsDivisible(int a, int b)
+        {
+            if (a % b == 0) return true;
+            return false;
+
+        }
+
+        public static bool IsEven(int a)
+        {
+            if (a % 2 == 0) return true;
+            else return false;
+        }
+
+        public static int IsInOrder(int a, int b)
+        {
+            if (a > b) return -1;
+            else if (a < b) return 1;
+            else return 0;
+        }
+
+        public static bool IsInOrderDesc(int arg1, int arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsListInOrder(List<int> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsMajeur(int age)
+        {
+            if (age < 18 && age > -1) return false; 
+            else if (age > 17 && age < 141) return true;
+            else throw new ArgumentException();
+        }
+
+        public static bool IsPrimary(int a)
+        {
+            for (int i = 2; i < a; i++) 
+            {
+                if (a % i == 0) return false;
+            }
+            return a > 1;
+        }
+
+        public static int Power(int a, int b)
+        {
+            int result = a;
+            for (int i = 1; i < b; i++)
+            {
+                result = result* a;
+            }
+            return result;
+        }
+
+        public static int Power2(int a)
+        {
+            return a * a;
+        }
+
+        public static List<int> Sort(List<int> toSort)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
